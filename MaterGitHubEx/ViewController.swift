6e405af20a156760f09f0575f1ffcb86b41c1193
Mannaiah ubbarapu <mannaiah@appmindsglobal.com>
2019-07-09 15:14:05 +0530

    Initial Commit

---
+++ MaterGitHubEx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaterGitHubEx
+//
+//  Created by Mac HD on 09/07/19.
+//  Copyright © 2019 karvy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
